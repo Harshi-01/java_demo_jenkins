09d51f3c32ba82fae150e7620605c8847503c631
Harshith Gowda <harshithn1903@gmail.com>
2024-09-20 15:33:43 +0530

    commit code

---
+++ src/main/java/com/deloitte/demo/App.java
@@ -0,0 +1,13 @@
+package com.deloitte.demo;
+
+/**
+ * @author hem
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
